f999d0fcd26afa7027d0141e3be666c0bdf12ecf
zakariahj2005 <90448269+zakariahj2005@users.noreply.github.com>
2023-01-31 13:50:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-/** Voeg hier je code toe */
+let click = 1;
+
+function addBlock() {
+
+    if (click <= 10) {
+        const myNode = document.createElement("div");
+        myNode.className = "block";
+        myNode.innerHTML = click;
+        document.getElementById("assignment-03").appendChild(myNode);
+
+        click++;
+
+    }
+    else {
+        alert("you can only add 10 blocks");
+    }
+
+
+}
